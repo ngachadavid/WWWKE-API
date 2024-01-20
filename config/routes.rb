3995b0840e26d89fa131ab3331e657c07c69ab2b
David <davidngacha01@gmail.com>
2024-01-20 13:16:22 +0300

    fix: configure article and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
